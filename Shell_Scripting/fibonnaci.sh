b34598386bf62e2052bfd17a5b982dcbe4defddb
Romeel R Chaudhari <romeel.chaudhari@gmail.com>
2020-06-15 23:14:11 -0700

    Added fibonnaci shell script

---
+++ Shell_Scripting/fibonnaci.sh
@@ -0,0 +1,23 @@
+# Program for Fibonacci 
+ 
+#total_Number 
+N=10
+
+#first_Number 
+first=0 
+
+# Second Number of the 
+# Fibonacci Series 
+second=1 
+
+echo "The Fibonacci series is as follows : "
+
+for (( i=0; i<N; i++ )) 
+do
+echo -n "$a "
+fn=$((first + second)) 
+a=$b 
+b=$fn 
+echo " "
+done
+				
